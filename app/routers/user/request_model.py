8541d6cf048b1bd48319f68ad5dfdecc655a82b9
Marcus <pths411027@gmail.com>
2023-09-12 10:09:44 +0800

    hhp exception

--- app/routers/user/request_model.py
+++ app/routers/user/request_model.py
@@ -1,10 +1,10 @@
-from typing import List
-
-from fastapi import File, HTTPException, UploadFile
-from pydantic import BaseModel, EmailStr, Field, validator
+from pydantic import BaseModel, Field
 
 
 class RegisterUser(BaseModel):
-    username: str = Field(..., example="Marcus Tsai")
-    password: str = Field(..., example="1234")
-    email: str = Field(..., example="marcus.tsai@shopp.com")
+    member_name: str = Field(..., example="Marcus Tsai")
+    member_photo: str = Field(..., example="oldman")
+    member_email: str = Field(..., example="Marcus.tsai")
+    member_email_type: str = Field(..., example="@shopee.com")
+    member_password: str = Field(..., example="1234")
+    member_level: str = Field(..., example="Entry")
